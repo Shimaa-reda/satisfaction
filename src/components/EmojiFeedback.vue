e03b9dde33af4922031033ecdf94abd89afa4c9c
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2025-05-19 11:38:22 +0300

    pop up

--- src/components/EmojiFeedback.vue
+++ src/components/EmojiFeedback.vue
@@ -2,17 +2,33 @@
   <div class="emoji-container">
     <h2 class="fw-bold question_Arabic">ما مدى رضاك عن تجربتك؟</h2>
     <h3 class="fw-bold question">How satisfied are you with <br> your experience?</h3>
+
     <div class="emojis">
-      <span @click="increaseCounter('veryAngryCounter')"><img src="../assets/veryangry.png" alt=""></span>
-      <span @click="increaseCounter('angryCounter')"><img src="../assets/angry.png" alt=""></span>
-      <span @click="increaseCounter('neutralCounter')"><img src="../assets/neutral.png" alt=""></span>
-      <span @click="increaseCounter('happyCounter')"><img src="../assets/happy.png" alt=""></span>
-      <span @click="increaseCounter('veryHappyCounter')"><img src="../assets/veryhappy.png" alt=""></span>
+      <span @click="handleEmojiClick('veryAngryCounter')"><img src="../assets/veryangry.png" alt=""></span>
+      <span @click="handleEmojiClick('angryCounter')"><img src="../assets/angry.png" alt=""></span>
+      <span @click="handleEmojiClick('neutralCounter')"><img src="../assets/neutral.png" alt=""></span>
+      <span @click="handleEmojiClick('happyCounter')"><img src="../assets/happy.png" alt=""></span>
+      <span @click="handleEmojiClick('veryHappyCounter')"><img src="../assets/veryhappy.png" alt=""></span>
     </div>
   </div>
 </template>
 
 <script setup>
+import Swal from 'sweetalert2'
+
+function handleEmojiClick(counterKey) {
+  increaseCounter(counterKey)
+
+  
+  Swal.fire({
+  html: '<div style="padding-bottom: 20px;">شكرا على تعليقك!<br> Thank you for your feedback!</div>',
+  icon: 'success',
+  showConfirmButton: false,
+  timer: 2000
+})
+
+}
+
 function increaseCounter(counterKey) {
   let current = localStorage.getItem(counterKey)
   current = current ? parseInt(current) : 0
@@ -25,11 +41,12 @@ function increaseCounter(counterKey) {
   font-family: "CustomArabicFont";
   src: url("@/assets/fonts/ExpoArabic-Bold.otf") format("truetype");
 }
-.question_Arabic{
+
+.question_Arabic {
   font-family: "CustomArabicFont", sans-serif;
   font-size: 40px;
-  
 }
+
 .emoji-container {
   text-align: center;
   font-family: sans-serif;
@@ -65,9 +82,6 @@ img {
   line-height: 1.5;
 }
 
-
-
-
 @media (min-width: 768px) and (max-width: 1024px) {
   .question {
     font-size: 36px;
@@ -82,14 +96,15 @@ img {
     height: 50px;
   }
 }
+
 @media (max-width: 767px) {
   .emoji-container {
     padding: 1rem;
     min-height: 100vh;
     display: flex;
     flex-direction: column;
-    justify-content: center; /* vertical centering */
-    align-items: center;     /* horizontal centering */
+    justify-content: center;
+    align-items: center;
     text-align: center;
   }
 
