101c4a7952575a00b0238b73e0586e405dd9e778
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2025-05-19 12:39:46 +0300

    localstorage after download

--- src/components/ExportCounters.vue
+++ src/components/ExportCounters.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="export-container">
     <h2>Download Feedback Summary</h2>
+
     <download-excel
+      ref="excelRef"
       :data="exportData"
       name="feedback-report.xlsx"
       :fields="fields"
       type="xlsx">
-      <button>Download Excel</button>
+      <button @click="handleExport">Download Excel</button>
     </download-excel>
   </div>
 </template>
 
-
 <script setup>
+import { ref } from 'vue'
 import JsonExcel from 'vue-json-excel3'
 
-const exportData = [
-  {
-    Very_Angry: localStorage.getItem('veryAngryCounter') || 0,
-    Angry: localStorage.getItem('angryCounter') || 0,
-    Neutral: localStorage.getItem('neutralCounter') || 0,
-    Happy: localStorage.getItem('happyCounter') || 0,
-    Very_Happy: localStorage.getItem('veryHappyCounter') || 0,
-  },
-]
+const excelRef = ref(null)
 
 const fields = {
   Very_Angry: 'Very_Angry',
@@ -33,9 +27,37 @@ const fields = {
   Very_Happy: 'Very_Happy',
 }
 
-console.log('Very Angry:', localStorage.getItem('veryAngryCounter'));
-console.log('Very Happy:', localStorage.getItem('veryHappyCounter'));
+const exportData = ref([
+  {
+    Very_Angry: localStorage.getItem('veryAngryCounter') || 0,
+    Angry: localStorage.getItem('angryCounter') || 0,
+    Neutral: localStorage.getItem('neutralCounter') || 0,
+    Happy: localStorage.getItem('happyCounter') || 0,
+    Very_Happy: localStorage.getItem('veryHappyCounter') || 0,
+  },
+])
 
+function handleExport() {
+  // Delay to ensure file downloaded
+  setTimeout(() => {
+    localStorage.setItem('veryAngryCounter', 0)
+    localStorage.setItem('angryCounter', 0)
+    localStorage.setItem('neutralCounter', 0)
+    localStorage.setItem('happyCounter', 0)
+    localStorage.setItem('veryHappyCounter', 0)
+
+   // Reset exportData too if the page remains open
+    exportData.value = [
+      {
+        Very_Angry: 0,
+        Angry: 0,
+        Neutral: 0,
+        Happy: 0,
+        Very_Happy: 0,
+      },
+    ]
+  }, 1000) // 1-second delay to download excel
+}
 </script>
 
 <script>
